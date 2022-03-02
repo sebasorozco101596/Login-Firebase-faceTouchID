b694edf63682912bbb9a2d6a069ea0b48be35a46
Juan S Orozco Buitrago <Sebasorozcob@gmail.com>
2022-03-01 23:25:15 -0500

    Initial Commit

---
+++ Login-Firebase-faceTouchID/Login_Firebase_faceTouchIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Login_Firebase_faceTouchIDApp.swift
+//  Login-Firebase-faceTouchID
+//
+//  Created by Juan Sebastian Orozco Buitrago on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Login_Firebase_faceTouchIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
